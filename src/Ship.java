a4d39c2be87b3100ca28e39272ebb8bd1575a190
Michiel De Koninck <dekoninckmichiel.mdk@gmail.com>
2017-03-07 21:57:28 +0100

    Afwerken van setters en standaardzaken. In principe af maar ongetwijfeld nog (schoonheids)fouten

--- src/Ship.java
+++ src/Ship.java
@@ -15,13 +15,12 @@ import be.kuleuven.cs.som.annotate.*;
  * @Invar 	The radius of each ship must be a valid value.
  * 			|isValidRadius(getRadius())
  * 
- *         
- *           
- *        
- * @note    The annotation @value indicates that objects of this class
- *          must be considered to be (immutable) values rather than
- *          mutable objects
- *          
+ * @Invar   The coordinates of a ship have to be finit numbers.
+ * 			|isValidCoordinate(getxPosition);
+ * 			|isValidCoordinate(getyPosition);       
+ *       
+
+ * @version 1.0     
  * @author James Defauw & Michiel De Koninck
  
  */
@@ -57,18 +56,18 @@ public class Ship {
 	 * 		   The orientation of this vessel, i.e., it's direction.
 	 * 		   Expressed in radians.    
 	 *       
-	 * @post   The lowest possible velocity is zero, it can only move forward.
-	 * 		   |new.getMinVelocity == 0
-	 * 
-	 * @post   The highest possible velocity is 300000 (km/s) it can never exceed this.
-	 * 		   |new.getMaxVelocity == 300000
-	 * 
-	 *   // NIET ZEKER DAT ONDERSTAANDE POST HIER THUISHOORT
-	 * @post   The lowest possible radius is 10 km. (This minimum can easily be changed).
-	 * 		   |new.getMinRadius == 10
-	 * 
-	 * @post   The orientation of the ship will always be a value between 0(Right) and 2PI(Right).
-	 * 		   |0 <= new.getOrientation < 2*PI
+//	 * @post   The lowest possible velocity is zero, it can only move forward.
+//	 * 		   |new.getMinVelocity == 0
+//	 * 
+//	 * @post   The highest possible velocity is 300000 (km/s) it can never exceed this.
+//	 * 		   |new.getMaxVelocity == 300000
+//	 * 
+//	 *   // NIET ZEKER DAT ONDERSTAANDE POST HIER THUISHOORT
+//	 * @post   The lowest possible radius is 10 km. (This minimum can easily be changed).
+//	 * 		   |new.getMinRadius == 10
+//	 * 
+//	 * @post   The orientation of the ship will always be a value between 0(Right) and 2PI(Right).
+//	 * 		   |0 <= new.getOrientation < 2*PI
 	 * 
 	 * @effect The given parameters are set as the properties of the new ship.
 	 * 		   |setPosition(xPosition,yPosition);
@@ -77,14 +76,15 @@ public class Ship {
 	 *	       |setOrientation(orientation);
 	 * 
 	 *@note    Any new ship initialized with this constructor
-     * 		   will satisfy all its class invariants.      
+     * 		   will satisfy all its class invariants. The setters will see to this in their implementation.
+     * 			      
 	 * 
 	 */
 	
 	// Position X and Y are described seperatly, this proves to be the easiest to work with. Same goes for speed.
 	public Ship(double xPosition, double yPosition, double xVelocity, double yVelocity, double radius, double orientation){
 		
-		// At this point we can invoke our mutators, because the range is known.
+		// At this point we can invoke our mutators. They will further impose the requirements posed above.
 		
 		setPosition(xPosition,yPosition);
 		setVelocity(xVelocity,yVelocity);
@@ -99,27 +99,27 @@ public class Ship {
 	 * Initialize this new ship with their parameters (position,speed,radius,orientation) set to their lowest possible Value.
 	 * 
 	 * @effect This new ship is initialized in the center of the grid: (0,0)
-	 * 			xPosition = 0
-	 * 			yPosition = 0
+	 * 			xPosition = 0.0
+	 * 			yPosition = 0.0
 	 * 			It's radius will be set to it's lowest possible value.
-	 * 			It's velocity will be set to 0. The ship will not be moving.
-	 * 			It's orientation is to the right, i.e.: it has an angle of 0 radians to the x-axis.
-	 * 
+	 * 			It's velocity will be set to it's minimal value: 0. The ship will not be moving.
+	 * 			It's orientation is to the right (Minimum value=0), i.e.: it has an angle of 0 radians to the x-axis.
+	 * 			|this(0.0,0.0,getMinVelocity(),getMaxVelocity(),Ship.getMinRadius(),Minimum_Orientation)
 	 * 			
 	 */
 	public Ship(){
-		this(0.0,0.0,0.0,0.0,Ship.getMinRadius(),0.0);
+		this(0.0,0.0,getMinVelocity(),getMaxVelocity(),Ship.getMinRadius(),Minimum_Orientation);
 	}
 	 
 	/**
 	 * Variable registering the xPosition of this Ship.
 	 */
-	private double xPosition = 0;
+	private double xPosition = 0.0;
 	
 	/**
 	 * Variable registering the yPosition of this Ship.
 	 */
-	private double yPosition = 0;
+	private double yPosition = 0.0;
 
 	
 	/**
@@ -136,44 +136,50 @@ public class Ship {
 	/**
 	 * Variable registering the orientation of this Ship.
 	 */
-	private double orientation = 0;
+	private double orientation = Minimum_Orientation;
 	
 	/**
 	 * Variable registering the radius of this Ship.
 	 */
 	private double radius = getMinRadius();
 	
+	/**
+	 * Variable registering the maximum allowed orientation
+	 */
+	private static double Maximum_Orientation = 2.0*Math.PI;
+	
+	/**
+	 * Variable registering the minimum allowed orientation
+	 */
+	private static double Minimum_Orientation = 0.0;
 	
 	
 
 // The inspectors 
 	
 	/**
-	 * Return the x-coordinate of this ship.
-	 * 
-	 * @return
+	 * Return the x-coordinate of this ship.  
+	 * @return the x-coordinate of this ship.
 	 */
 	@Basic
 	public double getxPosition(){
-		return this.xPosition;
-		
+		return this.xPosition;	
 	}	
 	
 
 	/**
 	 * Return the y-coordinate of this ship.
-	 * 
+	 * @return the y-coordinate of this ship.
 	 * 
 	 */
 	@Basic
 	public double getyPosition(){
-		return this.yPosition;
-		
+		return this.yPosition;	
 	}	
 	
 	/**
 	 * Return the velocity of this ship, in the x-direction.
-	 * 
+	 * @return the horizontal velocity of this ship.
 	 */
 	@Basic
 	public double getxVelocity(){
@@ -183,7 +189,7 @@ public class Ship {
 	
 	/**
 	 * Return the velocity of this ship, in the y-direction.
-	 * 
+	 * @return the vertical velocity of this ship.
 	 */
 	@Basic
 	public double getyVelocity(){
@@ -192,8 +198,12 @@ public class Ship {
 	}
 	
 	/**
-	 * Return the velocity of this ship, in the y-direction.
-	 * 
+	 * Return the total velocity of this ship.
+	 * @return the total velocity of this ship.
+	 * @note The total velocity of a ship is computed:
+	 * 		 by taking the square root of the sum of the
+	 * 		 secondpowers of the horizontal and vertical velocity.
+	 * 		-> sqrt(Vx^2+Vy^2)
 	 */
 	@Basic
 	public double getVelocity(){
@@ -204,67 +214,91 @@ public class Ship {
 	
 	/**
 	 *  Return the orientation of this ship.
-	 * @return
+	 * @return the orientation of this ship.
 	 */
 	@Basic
 	public double getOrientation(){
-		return this.orientation;
-		
+		return this.orientation;	
 	}	
 	
 	/**
-	 *  Return the Radius of this ship.
-	 * @return
+	 *  Return the radius of this ship.
+	 * @return the radius of this ship.
 	 */
-	@Basic @Immutable
+	@Basic
 	public double getRadius(){
 		return this.radius;
 	}
 	
 	/**
 	 * Returns the minimum Radius that a ship needs to have.
-	 *
+	 * 
+	 * @return The lowest possible radius for each ship must be non-negative.
+	 * 			| result >= 0
+	 * 
+	 * @note We have chosen to make this a static method,
+	 * 		 because all ships must have this boundary.
+	 * 		 
 	 */
 	@Basic
-	public static double getMinRadius() {
+	public static  double getMinRadius() {
 		return 10; 
 	}
 	
 	/**
 	 * Returns the minimum velocity that a ship can have.
-	 * @return
+	 * @return the minimum velocity that a ship can have.
 	 */
 	@Basic
-	public double getMinVelocity() {
+	public static double getMinVelocity() {
 		return 0;
 	}
 	
 	/**
 	 * Returns the maximum velocity that a ship can have.
-	 * @return
+	 * @return the maximum velocity that a ship can have.
 	 */
 	@Basic
-	public double getMaxVelocity() {
+	public static double getMaxVelocity() {
 		return 300000;
 	}
 	
 	
- 
-//-----------------------------------------------------------------------------------------------------------
 	
-// The Mutators... (Position: Defensively | Velocity: Total | Orientation: Nominally | Radius: Defensively)
-	
-	/**
+	/** DEFENSIVE PROGRAMMING
 	 * 
 	 * @param xPostion
 	 * 		  The new x-coordinate for this ship.	
 	 * @param yPosition
 	 * 		  The new y-coordinate for this ship.
+	 * @post The x-coordinate of the  ship equals the given xPosition.
+	 * 		 |new.getxPosition == xPosition
+	 * @post The y-coordinate of the  ship equals the given yPosition.
+	 * 		 |new.getyPosition == yPosition
 	 * 
-	 * @post 
+	 * @throws IllegalArgumentException
+	 * 		 The given xPosition or yPosition is not valid.
+	 * 	     | !isValidPosition(xPosition,yPosition)
 	 */
-	public void setPosition(double xPostion, double yPosition){
-		
+	public void setPosition(double xPosition, double yPosition) 
+			throws IllegalArgumentException{
+		if (!isValidPosition(xPosition,yPosition))
+			throw new IllegalArgumentException();
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+	}
+	
+	/**
+	 * @param xPostion
+	 * 		  The x-coordinate for this ship.	
+	 * @param yPosition
+	 * 		  The y-coordinate for this ship.	 
+	 * @return True if and only if neither of the coordinates is either infinity or NaN
+	 *		  | result == (xPosition != Double.NaN && yPosition != Double.NaN && xPosition != Double.POSITIVE_INFINITY && xPosition != Double.NEGATIVE_INFINITY && yPosition != Double.POSITIVE_INFINITY && yPosition != Double.NEGATIVE_INFINITY)
+	 *
+	 */
+	public static boolean isValidPosition(double xPosition, double yPosition){
+		return (xPosition != Double.NaN && yPosition != Double.NaN && xPosition != Double.POSITIVE_INFINITY && xPosition != Double.NEGATIVE_INFINITY && yPosition != Double.POSITIVE_INFINITY && yPosition != Double.NEGATIVE_INFINITY);
 	}
 	
 	/** TOTAL PROGRAMMING 
@@ -294,13 +328,14 @@ public class Ship {
 	 * 		 Everything that is not changed within the method is left  untouched.
 	 */
 	public void setVelocity(double xVelocity, double yVelocity){
+		// For readability, we computed total_velocity seperatly
 		double total_velocity = (Math.sqrt(Math.pow(getyVelocity(),2)+ Math.pow(getxVelocity(),2)));
 		
 		if ((total_velocity >= getMinVelocity()) &&
 			(total_velocity <= getMaxVelocity()))
 					xVelocity = getxVelocity();
 					yVelocity = getyVelocity();
-		else if (total_velocity > getMaxVelocity())
+		if (total_velocity > getMaxVelocity())
 					xVelocity = 150000*Math.sqrt(2);
 					yVelocity = 150000*Math.sqrt(2);
 		this.xVelocity = xVelocity;
@@ -310,17 +345,64 @@ public class Ship {
 	}
 	
 	
-	
-	public void setRadius(double Radius){
-		
+	/** Defensive Programming
+	 * 
+	 * @param radius
+	 * 		  The new, given radius of the ship.
+	 * 
+	 * @post The radius of the ship is now equal to the given radius.
+	 * 		|new.getRadius() == radius	
+	 * 
+	 * @throws  IllegalRadiusException
+	 * 		   The given radius is not a valid radius.
+	 * 		   | ! isValidRadius(radius)
+	 */
+	public void setRadius(double radius) throws IllegalArgumentException{
+		if (!isValidRadius(radius))
+			throw new IllegalArgumentException();
+		this.radius = radius;
 	}
 	
+	public static boolean isValidRadius(double radius){
+		return radius >= Ship.getMinRadius();
+	}
+	
+	
+	
+	/** Nominal Programming
+	 * 
+	 * @param orientation
+	 * 		  The new, given orientation of the ship.
+	 * @pre The given orientation must be a valid one.
+	 * 		|isValidOrientation(orientation)
+	 * @post The orientation of the ship is now changed to the given value
+	 *		|new.getOrientation()== orientation
+	 */
 	public void setOrientation(double orientation){
-		
+		assert isValidOrientation(orientation);
+		this.orientation = orientation;	
 	}
 	
+        
+    /**
+     * 
+     * @param orientations
+     * 		  The orientation of which we need to check wether it is legal.
+     * 
+     * @return True if and only if the given orientation is within the boundaries opposed upon orientation.
+     * 		   |result == (Minimum_Orientation <= orientation) && (orientation < Maximum_Orientation)
+     * 
+     */
+    public static boolean isValidOrientation(double orientation){
+    	return (Minimum_Orientation <= orientation) && (orientation <= Maximum_Orientation);	
+    }
+    	
+    			
+ 
+    
+ //-----------------JAMES----------------------------------------------------------------------
 	
-	//thrust is total programming
+    //thrust is total programming
 	public void thrust(double acceleration){
 		double a = Math.max(0, acceleration);
 		double xVelocity = 0 + a*(Math.cos(this.getOrientation()));
