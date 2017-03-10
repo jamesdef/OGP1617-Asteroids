40973c6d073ddcb7f051d5e92f8ccc450bd4e736
Michiel De Koninck <dekoninckmichiel.mdk@gmail.com>
2017-03-10 22:40:55 +0100

    Juist uitwerken van defensieve methoden. Verder uitwerken van documentatie

--- src/asteroids/model/Ship.java
+++ src/asteroids/model/Ship.java
@@ -1,28 +1,25 @@
 package asteroids.model;
-import java.util.Set;
-
 import be.kuleuven.cs.som.annotate.*;
 
 /**
  * A class for dealing with ships that have a certain position, radius, speed and orientation.
  * 
  * @invar 	The lowest possible velocity is zero, it can only move forward.
- * 			|new.getMinVelocity == 0
+ * 			|getxVelocity() >= 0
+ * 			|getyVelocity() >= 0
  * 
- * @invar   The highest possible velocity is 300000 (km/s) it can never exceed this.
- * 	      	|new.getMaxVelocity == 300000
- * 
+ * @invar   The highest possible velocity is 300000 (km/s) the ship can never exceed this speed.
+ * 	      	|!=exceedsMaxVelocity(getxVelocity(), getyVelocity())
+ *  * 
  * @invar	The orientation of the ship must be a valid value.
  * 			|isValidOrientation(getOrientation())
  * 
  * @invar 	The radius of each ship must be a valid value.
  * 			|isValidRadius(getRadius())
  * 
- * @invar   The coordinates of a ship have to be finit numbers.
- * 			|isValidCoordinate(getxPosition);
- * 			|isValidCoordinate(getyPosition);       
- *       
-
+ * @invar   The coordinates of a ship must be finit numbers.
+ * 			|isValidPosition(getxPosition,getyPosition);
+ *   
  * @version 1.0     
  * @author James Defauw & Michiel De Koninck
  
@@ -30,7 +27,7 @@ import be.kuleuven.cs.som.annotate.*;
 
 public class Ship {
 
-//Constructors: #2
+//-------------------------------   Constructors: #2 --------------------------------------------
 	/**
 	 * Initialize this new ship with given position,radius, speed and orientation.
 	 * 
@@ -58,19 +55,9 @@ public class Ship {
 	 * @param  orientation
 	 * 		   The orientation of this vessel, i.e., it's direction.
 	 * 		   Expressed in radians.    
-	 *       
-//	 * @post   The lowest possible velocity is zero, it can only move forward.
-//	 * 		   |new.getMinVelocity == 0
-//	 * 
-//	 * @post   The highest possible velocity is 300000 (km/s) it can never exceed this.
-//	 * 		   |new.getMaxVelocity == 300000
-//	 * 
-//	 *   // NIET ZEKER DAT ONDERSTAANDE POST HIER THUISHOORT
-//	 * @post   The lowest possible radius is 10 km. (This minimum can easily be changed).
-//	 * 		   |new.getMinRadius == 10
-//	 * 
-//	 * @post   The orientation of the ship will always be a value between 0(Right) and 2PI(Right).
-//	 * 		   |0 <= new.getOrientation < 2*PI
+	 * 
+	 * @throws IllegalPositionException 
+	 * @throws IllegalRadiusException 
 	 * 
 	 * @effect The given parameters are set as the properties of the new ship.
 	 * 		   |setPosition(xPosition,yPosition);
@@ -80,40 +67,43 @@ public class Ship {
 	 * 
 	 *@note    Any new ship initialized with this constructor
      * 		   will satisfy all its class invariants. The setters will see to this in their implementation.
-     * 			      
-	 * 
 	 */
 	
-	// Position X and Y are described seperatly, this proves to be the easiest to work with. Same goes for speed.
-	public Ship(double xPosition, double yPosition, double xVelocity, double yVelocity, double radius, double orientation){
+	// Position X and Y are described seperatly, this proves to be the easiest to work with. Same goes for velocity.
+	public Ship(double xPosition, double yPosition, double xVelocity, double yVelocity, double radius, double orientation) throws IllegalPositionException, IllegalRadiusException{
 		
-		// At this point we can invoke our mutators. They will further impose the requirements posed above.
-		
+		// At this point we can invoke our mutators. They will see to it that the class invariants hold at all times.
 		setPosition(xPosition,yPosition);
 		setVelocity(xVelocity,yVelocity);
 		setRadius(radius);
-		setOrientation(orientation);
-		
+		setOrientation(orientation);	
 	}
 	
 	
-
 	/**
-	 * Initialize this new ship with their parameters (position,speed,radius,orientation) set to their lowest possible Value.
+	 * Initialize this new ship with their parameters (position,speed,radius,orientation) set to their lowest possible values.
+	 * @throws IllegalPositionException 
+	 * @throws IllegalRadiusException 
 	 * 
-	 * @effect This new ship is initialized in the center of the grid: (0,0)
-	 * 			xPosition = 0.0
-	 * 			yPosition = 0.0
+	 * @effect 	This new ship is initialized in the center of the grid: (0,0)
 	 * 			It's radius will be set to it's lowest possible value.
 	 * 			It's velocity will be set to it's minimal value: 0. The ship will not be moving.
 	 * 			It's orientation is to the right (Minimum value=0), i.e.: it has an angle of 0 radians to the x-axis.
-	 * 			|this(0.0,0.0,getMinVelocity(),getMaxVelocity(),Ship.getMinRadius(),Minimum_Orientation)
+	 * 			|this.xPosition = 0.0
+	 * 			|this.yPosition = 0.0
+	 * 			|this.getxVelocity = Min_Velocity
+	 * 			|this.getyVelocity = Max_Velocity
+	 * 			|this.getOrientation = Min_Orientation
+	 * 			|this.getRadius = Min_Radius
+	 * i.e. :   |this(0.0,0.0,Min_Velocity,Max_Velocity,Min_Radius,Minimum_Orientation);
 	 * 			
 	 */
-	public Ship(){
+	public Ship() throws IllegalPositionException, IllegalRadiusException{
 		this(0.0,0.0,Min_Velocity,Max_Velocity,Min_Radius,Minimum_Orientation);
 	}
 	 
+	// -----------------------  VARIABLES (DEFAULTS & FINAL) ----------------------------------- 
+	
 	/**
 	 * Variable registering the xPosition of this Ship.
 	 */
@@ -136,29 +126,49 @@ public class Ship {
 	 */
 	private double yVelocity = Min_Velocity;
 	
+	
+	/**
+	 * Variable registering the minimum allowed velocity.
+	 */
+	private static final double Min_Velocity = 0;
+	
+	/**
+	 * Variable registering the maximum allowed velocity.
+	 */
+	private static final double Max_Velocity = 300000;
+	
 	/**
 	 * Variable registering the orientation of this Ship.
 	 */
 	private double orientation = Minimum_Orientation;
 	
 	/**
-	 * Variable registering the radius of this Ship.
+	 * Variable registering the minimum allowed orientation
 	 */
-	private double radius = Min_Radius;
+	private static final double Minimum_Orientation = 0.0;
+	
 	
 	/**
 	 * Variable registering the maximum allowed orientation
 	 */
-	private static double Maximum_Orientation = 2.0*Math.PI;
+	private static final double Maximum_Orientation = 2.0*Math.PI;
+	
 	
 	/**
-	 * Variable registering the minimum allowed orientation
+	 * Variable registering the radius of this Ship.
 	 */
-	private static double Minimum_Orientation = 0.0;
+	private double radius = Min_Radius;
+
+	/**
+	 * Variable registering the minimum allowed Radius.
+	 */
+	private static final double Min_Radius = 10;
+
+
 	
 	
 
-// The inspectors 
+// ------------------------   The inspectors -------------------------------------------------------
 	
 	/**
 	 * Return the x-coordinate of this ship.  
@@ -210,8 +220,7 @@ public class Ship {
 	 */
 	@Basic
 	public double getVelocity(){
-		return Math.sqrt(Math.pow(getyVelocity(),2.0)+Math.pow(getxVelocity(),2.0));
-		
+		return Math.sqrt(Math.pow(getyVelocity(),2.0)+Math.pow(getxVelocity(),2.0));	
 	}
 	
 	
@@ -233,58 +242,16 @@ public class Ship {
 		return this.radius;
 	}
 	
-// STEPHEN HUANG: Toch best om geen getMethodes aan te maken voor zulk een simpele dingen. Gewoon variabele maken.
-	
-//	/**
-//	 * Returns the minimum Radius that a ship needs to have.
-//	 * 
-//	 * @return The lowest possible radius for each ship must be non-negative.
-//	 * 			| result >= 0
-//	 * 
-//	 * @note We have chosen to make this a static method,
-//	 * 		 because all ships must have this boundary.
-//	 * 		 
-//	 */
-//	@Basic
-//	public static  double getMinRadius() {
-//		return 10; 
-//	}
-//	
-//	/**
-//	 * Returns the minimum velocity that a ship can have.
-//	 * @return the minimum velocity that a ship can have.
-//	 */
-//	@Basic
-//	public static double getMinVelocity() {
-//		return 0;
-//	}
-//	
-//	/**
-//	 * Returns the maximum velocity that a ship can have.
-//	 * @return the maximum velocity that a ship can have.
-//	 */
-//	@Basic
-//	public static double getMaxVelocity() {
-//		return 300000;
-//	}
-	
-	private static double Min_Radius = 10;
-	
-	private static double Min_Velocity = 0;
-	
-	private static double Max_Velocity = 300000;
-
-	
-	/**
+	/** 
+	 *  Returns the x and y coordinates within an array.
+	 *  
 	 * @return the x and y coordinate as an array.
 	 * 
 	 */
 	public double[] getPosition(){
 		
 		double[] position = {this.getxPosition(),this.getyPosition()};	
-		return position;
-	
-				
+		return position;			
 	}
 	
 	
@@ -301,14 +268,14 @@ public class Ship {
 	 * @post The y-coordinate of the  ship equals the given yPosition.
 	 * 		 |new.getyPosition == yPosition
 	 * 
-	 * @throws IllegalArgumentException
+	 * @throws IllegalPositonException
 	 * 		 The given xPosition or yPosition is not valid.
 	 * 	     | !isValidPosition(xPosition,yPosition)
 	 */
 	public void setPosition(double xPosition, double yPosition) 
-			throws IllegalArgumentException{
+			throws IllegalPositionException{
 		if (!isValidPosition(xPosition,yPosition))
-			throw new IllegalArgumentException();
+			throw new IllegalPositionException(xPosition,yPosition);
 		this.xPosition = xPosition;
 		this.yPosition = yPosition;
 	}
@@ -333,21 +300,20 @@ public class Ship {
 	 * @param yVelocity
 	 * 		  The new velocity in the y-direction for this ship.
 	 * 
-	 * @post If the given velocity is within the range of acceptable velocities,
-	 * 		 the  velocity of this ship is equal to the given velocity.
-	 * 		|if ((sqrt(xVelocity^2 + yVelocity^2) >= getMinVelocity()) &&
-	 * 			((sqrt(xVelocity^2 + yVelocity^2)<= getMaxVelocity())))
-	 * 		|	then new.getxVelocity() == xVelocity && new.getyVelocity() == yVelocity
+	 *@post If the new total velocity does not exceed the maximum velocity
+	 *		and both components are postive then the given values are set as the new velocity.
+	 *		|if (!new.exceedsMaxVelocity() & hasPositiveComponents())
+	 *		|		then new.xVelocity = xVelocity;
+	 *   				 new.yVelocity = yVelocity;
 	 * 
-	 * @post If the total velocity exceeds the maximum velocity, 
-	 * 		 both velocities are set to be the same and at their maximum value.
-	 * 		|if ((sqrt(xVelocity^2 + yVelocity^2) > getMaxVelocity())))
-	 * 			then new.getxVelocity() =new.getyVelocity() = 150000*sqrt(2)
+	 * @post If the new total velocity exceeds the maximum velocity, 
+	 * 		 both velocities scaled so that the class invariants hold.
+	 * 		|if (new.exceedsMaxVelocity()) 
+	 * 		|   then new.scaleVelocity()
 	 * 
-	 * @post If either of the given velocities is below the minimum value,
-	 * 		 both velocities are left untouched.
-	 * 		|if (xVelocity < getMinVelocity() or yVelocity < getMinVelocity())
-	 * 		|	then new.getxVelocity = this.getyVelocity() && new.getxVelocity() = this.getyVelocity()
+	 * @post If the given velocity has negative components, nothing is changed.
+	 * 		|if !hasPositiveComponents(xVelocity, yVelocity)
+	 * 		|    then new.getVelocity() = this.getVelocity()
 	 * 
 	 * @note The last postcondition is not really necessary.
 	 * 		 Everything that is not changed within the method is left  untouched.
@@ -363,43 +329,67 @@ public class Ship {
 			if(this.exceedsMaxVelocity(xVelocity, yVelocity)){
 				this.scaleVelocity(xVelocity, yVelocity);
 			}
-			
 		}
-		
 	} 
-	
-//		// For readability, we computed total_velocity seperatly
-//		double total_velocity = (Math.sqrt(Math.pow(getyVelocity(),2)+ Math.pow(getxVelocity(),2)));
-//		
-//		if ((total_velocity >= Min_Velocity) &&
-//			(total_velocity <= Max_Velocity))
-//					xVelocity = getxVelocity();
-//					yVelocity = getyVelocity();
-//		if (total_velocity > Max_Velocity)
-//					xVelocity = 150000*Math.sqrt(2);
-//					yVelocity = 150000*Math.sqrt(2);
-//		this.xVelocity = xVelocity;
-//		this.yVelocity = yVelocity;
 					
 		
 	
-	
+	/**
+	 * Check whether the velocity has only positive components.
+	 * @param xVelocity
+	 * 		  The velocity in the x-direction
+	 * @param yVelocity
+	 * 		  The velocity in the y-direction
+	 * 
+	 * @return If either of the velocities is negative, the method returns false.
+	 * 		   Otherwise it will return true.
+	 * 		   |(xVelocity >=0) && (yVelocity >=0); 
+	 */
 	public boolean hasPositiveComponents(double xVelocity, double yVelocity){
-		return (xVelocity >=0) && (xVelocity >=0);
+		return (xVelocity >=0) && (yVelocity >=0);
 	}
 	
+	/**
+	 * Check whether the velocity exceeds the maximum allowed velocity.
+	 * 
+	 * @param xVelocity
+	 * 		  The velocity in the x-direction
+	 * @param yVelocity
+	 *        The velocity in the y-direction
+	 *        
+	 * @return if the total velocity (which is computed
+	 * 		  by taking the square root of the sum of the
+	 * 		  secondpowers of the horizontal and vertical velocity.
+	 * 		 -> sqrt(Vx^2+Vy^2)) exceeds the maximum. This returns true.
+	 * 		 Otherwise it will return false.
+	 * 		 | (Math.sqrt(Math.pow(getyVelocity(),2)+Math.pow(getxVelocity(),2)) > Max_Velocity);	
+	 */
 	public boolean exceedsMaxVelocity(double xVelocity, double yVelocity){
 		return (Math.sqrt(Math.pow(getyVelocity(),2)+Math.pow(getxVelocity(),2)) > Max_Velocity);
 		
 	}
 	
+	/** Changes the velocity to it's scaled value (so that the class invariants hold)
+	 *  .
+	 * 
+	 * @param xVelocity
+	 * 		  The velocity in the x-direction
+	 * @param yVelocity
+	 *        The velocity in the y-direction
+	 * 
+	 * @note This method is only called upon if the total velocity of the given parameters exceed the maximum velocity.
+	 * 		 
+	 * @post The velocities are changed to their scaled values. 
+	 * 		 The velocity no longer exceeds the limit.
+	 * 		| new.xVelocity = (xVelocity*Max_Velocity)/this.getVelocity();
+	 *      | new.yVelocity = (yVelocity*Max_Velocity)/this.getVelocity();	     
+	 */
 	public void scaleVelocity(double xVelocity, double yVelocity ){
 		double scaledxVelocity = (xVelocity*Max_Velocity)/this.getVelocity();
 		double scaledyVelocity = (yVelocity*Max_Velocity)/this.getVelocity();
 		
 		this.xVelocity = scaledxVelocity;
 		this.yVelocity = scaledyVelocity;
-		
 	}
 	
 	
@@ -415,12 +405,21 @@ public class Ship {
 	 * 		   The given radius is not a valid radius.
 	 * 		   | ! isValidRadius(radius)
 	 */
-	public void setRadius(double radius) throws IllegalArgumentException{
+	public void setRadius(double radius) throws IllegalRadiusException{
 		if (!isValidRadius(radius))
-			throw new IllegalArgumentException();
+			throw new IllegalRadiusException(radius);
 		this.radius = radius;
 	}
 	
+	/** 
+	 * 
+	 * @param  radius
+	 * 		   The radius of the ship.
+	 * 
+	 * @return True if the radius exceeds the minimal radius
+	 * 		   false if the radius is less than the minimal_radius.
+	 * 		   | radius >= Min_Radius;
+	 */
 	public static boolean isValidRadius(double radius){
 		return radius >= Min_Radius;
 	}
@@ -459,25 +458,47 @@ public class Ship {
  
     
  //----------------- Moving, turning and accelerating----------------------------------------------------------------------
-	
+    
+	/** Total Programming
+	 * 
+	 * @param acceleration
+	 * 
+	 * @post if the given acceleration is negative
+	 * 		 The velocity is left untouched
+	 * 		 |if acceleration < 0
+	 * 		 | 		then new.getxVelocity() = getxVelocity()
+	 * 	     | 		then new.getyVelocity() = getyVelocity()
+	 * 
+	 * @post if the given acceleration is greater than zero, the new velocities
+	 * 		 are computed as follows:
+	 * 		 |NewxVelocity = this.getxVelocity() + a*(Math.cos(this.getOrientation()));
+		     |NewyVelocity = this.getyVelocity() + a*(Math.sin(this.getOrientation()));
+		
+	 * @effect The velocity is changed, dependent on the given acceleration.
+	 * 	      This is only the case if the calculated new velocity is legal.
+	 * 		  The method setVelocity makes sure that this is the case.
+		
+	 * 		  |this.setVelocity(NewxVelocity, NewyVelocity);
+	 */
 	public void thrust(double acceleration){
+		//If the acceleration is negative, then we leave the velocity untouched.
 		double a = Math.max(0, acceleration);
-		double xVelocity = this.getxVelocity() + a*(Math.cos(this.getOrientation()));
-		double yVelocity = this.getyVelocity() + a*(Math.sin(this.getOrientation()));
+		double NewxVelocity = this.getxVelocity() + a*(Math.cos(this.getOrientation()));
+		double NewyVelocity = this.getyVelocity() + a*(Math.sin(this.getOrientation()));
 		
-		
-		this.setVelocity(xVelocity, yVelocity);
-		
-		
+		this.setVelocity(NewxVelocity, NewyVelocity);	
 	}
 	
 	/** Defensive programming
 	 * 
 	 * @param duration
-	 * 			The duration of the movement, given in seconds
-	 * @pre The duration is non-negative 
+	 * 		  The duration of the movement, given in seconds
+	 * 
+	 * @throws isValidDuration
+	 * @throws IllegalPositionException 
+	 * 
 	 */
-	public void move(double duration){
+	public void move(double duration) throws IllegalPositionException{
 		
 		double newxPosition = this.getxPosition() + (duration)*(this.getxVelocity());
 		double newyPosition = this.getyPosition() + (duration)*(this.getyVelocity());
@@ -487,10 +508,13 @@ public class Ship {
 	}
 	
 	
+	
 	/** Nominal programming
 	 * 
 	 * 
 	 * @param angle
+	 * 
+	 * @pre
 	 */
 	public void turn(double angle){
 		double newAngle = this.getOrientation() + angle;
@@ -498,7 +522,7 @@ public class Ship {
 	}
 
 	
-//  COLLISION PREDICTION 
+//  COLLISION PREDICTION  : DEFENSIVE
 	
 	public double getDistanceBetween(Ship ship){
 		double centerDistance = Math.sqrt(Math.pow((this.getxPosition()-ship.xPosition), 2.0)+ Math.pow((this.getyPosition()-ship.yPosition), 2.0));
@@ -519,6 +543,8 @@ public class Ship {
 	 * 
 	 * @return Returns in how many seconds 2 spacecrafts will collide.
 	 * 		   If they never collide it wil return Double.POSITIVE_INFINITY
+	 * 
+	 * @throws If two ships overlap, this method does not apply.
 	 */
 	public double getTimeToCollision(Ship ship){
 		double sigma = Math.sqrt(Math.pow((this.getxPosition()-ship.xPosition), 2.0)+ Math.pow((this.getyPosition()-ship.yPosition), 2.0));
@@ -547,9 +573,34 @@ public class Ship {
 		//For this we first calculate where the two ships are at, at the time of collision.
 		//Then we calculate where exactly they collide.
 		
-		double 
+		double T = getTimeToCollision(ship);
 		
+		if (T == Double.POSITIVE_INFINITY){
+			return null;
+		}
 		
-	}
+		//Where are the ships after time T?
+		
+		double[] FirstShipPosition = {this.getxPosition() + this.getxVelocity()*T, this.getyPosition() + this.getyVelocity()*T};
+		double[] SecondShipPosition = {ship.getxPosition() + ship.getxVelocity()*T, ship.getyPosition() + ship.getyVelocity()*T};
+		
+		//The position of the first ship, incremented with it's radius (in the right direction = direction to the center of the other ship) gives the answer
+		
+		double[] CenterDistance = {SecondShipPosition[0] - FirstShipPosition[0], SecondShipPosition[1]- FirstShipPosition[1]};
+		
+		double Norm = Math.sqrt(Math.pow(CenterDistance[0],2.0)+ Math.pow(CenterDistance[1],2.0));
+		
+		double[] NormedCenterDistance = {(SecondShipPosition[0] - FirstShipPosition[0])/Norm, (SecondShipPosition[1]- FirstShipPosition[1])/Norm};
+		
+		double[]RadiusWithDirection = {this.getRadius()*NormedCenterDistance[0],this.getRadius()*NormedCenterDistance[1]};
+		
+		double[] CollisionCoordinates = {FirstShipPosition[0]+RadiusWithDirection[0], FirstShipPosition[1]+RadiusWithDirection[1]};
+		
+		return CollisionCoordinates;
+		
+		}
+		// TIP: Maak een klasse position aan met argumenten x en y zodat je er zeker van bent dat een position altijd 2ledig is.
+		// In deze klasse zou je dan bv een methode kunnen maken voor de optelling van een position en een vector voor het maken van een nieuwe position.
+		
 	
 }
