0577f0b267cfdf6c0394060be1bf495f46acd92b
Michiel De Koninck <dekoninckmichiel.mdk@gmail.com>
2017-03-24 17:54:10 +0100

    emmerse

--- src/asteroids/model/Entity.java
+++ src/asteroids/model/Entity.java
@@ -1,5 +1,19 @@
 package asteroids.model;
 
+// The goal is to create a class that involves all the thinggs that entities (ie ships and bullets)
+// have in common. Bullet and ship will then be made subclasses of Entity. If needed, the methods within
+// these classes can override the methods of ENtity.
+
+/** Plaats, snelheid, standaard methoden,...
+ *  
+ * 
+ * @author Michiel
+ *
+ */
+
 public class Entity {
+	
+	public Entity(double xPosition, double yPosition, double xVelocity, double yVelocity, double radius, double orientation){ 
+	
 
 }
